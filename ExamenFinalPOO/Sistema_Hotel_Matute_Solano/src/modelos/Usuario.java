dac2aff47c2159367907b1bdba2ec4e3038ded68
AndresS505-star <sucoshebas@gmail.com>
2025-07-21 18:50:48 -0500

    Update Usuario.java

--- ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/Usuario.java
+++ ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/Usuario.java
@@ -1,1 +1,19 @@
+package modelo;
 
+public abstract class Usuario {
+    protected String nombre;
+    protected String apellido;
+    protected String usuario;
+    protected String contraseña;
+
+    public Usuario(String nombre, String apellido, String usuario, String contraseña) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+
+    public abstract String getTipoUsuario();
+
+    // Getters y setters
+}
