99ad796bdd4f17161441c35d4fac2ac473813270
AndresS505-star <sucoshebas@gmail.com>
2025-07-21 18:52:31 -0500

    Update Cliente.java

--- ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/Cliente.java
+++ ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/Cliente.java
@@ -1,1 +1,17 @@
+package modelo;
 
+public class Cliente {
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+
+    public Cliente(String cedula, String nombre, String apellido, String telefono) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+
+    // Getters, setters, toString
+}
