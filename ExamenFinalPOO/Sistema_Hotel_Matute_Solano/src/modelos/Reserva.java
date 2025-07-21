0ae4ad3e9d99e17d21a55ff4adb8120cf7ff8c35
AndresS505-star <sucoshebas@gmail.com>
2025-07-21 18:53:08 -0500

    Update Reserva.java

--- ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/Reserva.java
+++ ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/Reserva.java
@@ -1,1 +1,30 @@
+package modelo;
 
+import java.time.LocalDate;
+
+public class Reserva implements Pago {
+    private Cliente cliente;
+    private Habitacion habitacion;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+
+    public Reserva(Cliente cliente, Habitacion habitacion, LocalDate fechaInicio, LocalDate fechaFin) {
+        this.cliente = cliente;
+        this.habitacion = habitacion;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+
+    @Override
+    public double calcularTotal() {
+        long dias = fechaFin.toEpochDay() - fechaInicio.toEpochDay();
+        double precioPorDia = switch (habitacion.getTipo()) {
+            case INDIVIDUAL -> 50;
+            case DOBLE -> 80;
+            case SUITE -> 120;
+        };
+        return dias * precioPorDia;
+    }
+
+    // Getters, setters, toString
+}
