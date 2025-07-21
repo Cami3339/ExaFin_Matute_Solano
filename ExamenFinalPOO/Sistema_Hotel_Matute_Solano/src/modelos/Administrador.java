00815682a571027cbe56ad681c82968ad9cfe9e4
AndresS505-star <sucoshebas@gmail.com>
2025-07-21 18:51:48 -0500

    Update Administrador.java

--- ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/Administrador.java
+++ ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/Administrador.java
@@ -1,1 +1,12 @@
+package modelo;
 
+public class Administrador extends Usuario {
+    public Administrador(String nombre, String apellido, String usuario, String contraseña) {
+        super(nombre, apellido, usuario, contraseña);
+    }
+
+    @Override
+    public String getTipoUsuario() {
+        return "Administrador";
+    }
+}
