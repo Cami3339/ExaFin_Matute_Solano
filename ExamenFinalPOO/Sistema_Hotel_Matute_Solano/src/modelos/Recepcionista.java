654b09740074b09e64c183b7752da69b4c7d8ab8
AndresS505-star <sucoshebas@gmail.com>
2025-07-21 18:51:29 -0500

    Update Recepcionista.java

--- ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/Recepcionista.java
+++ ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/Recepcionista.java
@@ -1,1 +1,12 @@
+package modelo;
 
+public class Recepcionista extends Usuario {
+    public Recepcionista(String nombre, String apellido, String usuario, String contraseña) {
+        super(nombre, apellido, usuario, contraseña);
+    }
+
+    @Override
+    public String getTipoUsuario() {
+        return "Recepcionista";
+    }
+}
