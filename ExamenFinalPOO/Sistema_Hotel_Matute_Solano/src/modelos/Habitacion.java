a7fe0aa6a8bd8cbf82a5eb9367d96d6ede402176
AndresS505-star <sucoshebas@gmail.com>
2025-07-21 18:52:11 -0500

    Update Habitacion.java

--- ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/Habitacion.java
+++ ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/Habitacion.java
@@ -1,1 +1,15 @@
+package modelo;
 
+public class Habitacion {
+    private int numero;
+    private TipoHabitacion tipo;
+    private boolean disponible;
+
+    public Habitacion(int numero, TipoHabitacion tipo, boolean disponible) {
+        this.numero = numero;
+        this.tipo = tipo;
+        this.disponible = disponible;
+    }
+
+    // Getters, setters, toString
+}
