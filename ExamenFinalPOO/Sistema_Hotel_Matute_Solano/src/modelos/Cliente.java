8907e3cd5755fbf6f3347a3cf02ae735ea3ed768
AndresS505-star <sucoshebas@gmail.com>
2025-07-21 20:17:11 -0500

    Update Cliente.java

--- ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/Cliente.java
+++ ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/Cliente.java
@@ -15,5 +15,43 @@ public class Cliente implements Serializable {
         this.telefono = telefono;
     }
 
-    // Getters, setters, toString
+    // Getters
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    // Setters
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellido + " (Cédula: " + cedula + ")";
+    }
 }
+
