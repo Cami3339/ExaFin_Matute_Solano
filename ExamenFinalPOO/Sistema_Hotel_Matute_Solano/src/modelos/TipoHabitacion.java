c617daceb6d8220e7c19d0d498df1da863d1a791
AndresS505-star <sucoshebas@gmail.com>
2025-07-22 18:56:42 -0500

    Update TipoHabitacion.java

--- ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/TipoHabitacion.java
+++ ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/TipoHabitacion.java
@@ -1,5 +1,9 @@
+// Paquete donde se agrupan las clases y enumeraciones del modelo
 package modelos;
 
+// Enumeración que define los tipos posibles de habitación en el sistema
 public enum TipoHabitacion {
-    INDIVIDUAL, DOBLE, SUITE;
+    INDIVIDUAL, // Habitación para una persona
+    DOBLE,      // Habitación para dos personas
+    SUITE       // Habitación tipo suite, más grande y lujosa
 }
