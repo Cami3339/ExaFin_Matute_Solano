5b76848fad2ad1539c8297c68ae93eee0466782a
AndresS505-star <sucoshebas@gmail.com>
2025-07-22 18:55:07 -0500

    Update Pago.java

--- ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/Pago.java
+++ ExamenFinalPOO/Sistema_Hotel_Matute_Solano/src/modelos/Pago.java
@@ -1,5 +1,9 @@
+// Paquete donde se agrupan las clases e interfaces del modelo
 package modelos;
 
+// Interfaz que define el contrato para el cálculo de pagos o totales
 public interface Pago {
+    // Método que debe implementar cualquier clase que maneje pagos
+    // Debe retornar el total calculado como un valor double
     double calcularTotal();
 }
